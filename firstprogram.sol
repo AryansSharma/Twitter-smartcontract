e6400b12988bfb9b58e86abd05fbc09b671528d0
AryansSharma <158320463+AryansSharma@users.noreply.github.com>
2024-02-09 00:39:26 +0530

    Create firstprogram.sol
    its a kind of twitter smart contract in which we can send the message to the follower whom you gave permission by using addresses.

---
+++ firstprogram.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.12 <0.9.0;
+
+contract Twitter {
+ 
+ 
+ struct send{
+  address value1;
+  string content;
+ }
+ 
+ send public s1;
+  mapping(address=>mapping(address=>bool))public permission;
+  function givingaccess(address key, address value, string calldata _content,bool confirmation) public returns(uint){
+    if(confirmation==true){
+          permission [key] [value] = true;
+          s1.value1 = value;
+          s1.content = _content;
+
+    }
+          return block.timestamp;  
+    }
+  function denyaccess(address key, address value) public returns(uint){
+        permission [key] [value] = false;
+        return block.timestamp;
+  }
+  function resenddata(address key, address value, string calldata _content) public {
+    s1.value1=value;
+    if( permission [key] [value] = true){
+      s1.content=_content;
+    }
+
+  }
+
+}
+      
